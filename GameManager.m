6f23575e559c8f4d70792f0554889db7116e83d1
talex7 <talexanian@gmail.com>
2016-11-28 15:26:12 -0500

    another commit

--- GameManager.m
+++ GameManager.m
@@ -12,6 +12,30 @@
 
 -(void)startGame{
     self.currentGame = [Game new];
+    NSMutableDictionary *dict1 = [NSMutableDictionary
+                                  dictionaryWithDictionary:@{@"20" : @0,
+                                                             @"19" : @0,
+                                                             @"18" : @0,
+                                                             @"17" : @0,
+                                                             @"16" : @0,
+                                                             @"15" : @0,
+                                                             @"Bull" : @0,
+                                                             @"Miss" : @0}];
+    
+    NSMutableDictionary *dict2 = [NSMutableDictionary
+                                  dictionaryWithDictionary:@{@"20" : @0,
+                                                             @"19" : @0,
+                                                             @"18" : @0,
+                                                             @"17" : @0,
+                                                             @"16" : @0,
+                                                             @"15" : @0,
+                                                             @"Bull" : @0,
+                                                             @"Miss" : @0}];
+    self.closedRooms = [NSMutableArray arrayWithObjects:
+                        dict1, dict2, nil];
+    self.playerScore = 0;
+    self.opponentScore = 0;
+    
 }
 
 
