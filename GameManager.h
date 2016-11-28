26f653290aeeacf7b6a9fdf8f52734796c58509e
talex7 <talexanian@gmail.com>
2016-11-28 12:15:00 -0500

    Game Manager class added and core data classes set up

---
+++ GameManager.h
@@ -0,0 +1,13 @@
+//
+//  GameManager.h
+//  Scoreboard
+//
+//  Created by Thomas Alexanian on 2016-11-28.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GameManager : NSObject
+
+@end
---
+++ GameManager.m
@@ -0,0 +1,13 @@
+//
+//  GameManager.m
+//  Scoreboard
+//
+//  Created by Thomas Alexanian on 2016-11-28.
+//
+//
+
+#import "GameManager.h"
+
+@implementation GameManager
+
+@end
