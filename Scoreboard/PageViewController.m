1cb9a08753e0ab107cb23b1ebf5e6fed3c4f812f
Matt Mauro <matt.mauro@gmail.com>
2016-11-28 17:12:24 -0500

    most buttons positioned

--- Scoreboard/PageViewController.m
+++ Scoreboard/PageViewController.m
@@ -2,14 +2,13 @@
 //  PageViewController.m
 //  Scoreboard
 //
-//  Created by Matthew Mauro on 2016-11-28.
+//  Created by Matthew Mauro and Tommy Alexian
 //
 //
 
 #import "PageViewController.h"
 #import "PlayerViewController.h"
 #import "GameSummaryViewController.h"
-
 
 @interface PageViewController ()
 
@@ -19,7 +18,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.dataSource = self;
+    
+    PlayerViewController *board = [self.storyboard instantiateViewControllerWithIdentifier:@"PlayerContent"];
+    [self.viewCs addObject:board];
+    GameSummaryViewController *gameSummary = [self.storyboard instantiateViewControllerWithIdentifier:@"GameSummary"];
+    [self.viewCs addObject:gameSummary];
+    
+    self.playerViewController = (PlayerViewController*)[self viewControllerAtIndex:0];
+    self.gameSummary = (GameSummaryViewController*)[self viewControllerAtIndex:1];
+    
+    //    [self addChildViewController:self.playerViewController];
+    [self setViewControllers:@[self.playerViewController] direction:UIPageViewControllerNavigationDirectionForward animated:YES completion:nil];
+    
 }
 
 - (void)didReceiveMemoryWarning {
@@ -54,8 +65,7 @@
     if ((index == 0)||(index == 3)){
         return nil;
     }
-    
-    if (index  <= 1) {
+    if (index  <= 0) {
         PlayerViewController *playerControl = [self.storyboard instantiateViewControllerWithIdentifier:@"BoardController"];
         playerControl.pageIndex = index;
         playerControl.players = self.players;
@@ -67,7 +77,6 @@
         return gameSummary;
     }
 }
-
 - (NSInteger)presentationCountForPageViewController:(UIPageViewController *)pageViewController
 {
     return 2;
@@ -76,7 +85,5 @@
 {
     return 0;
 }
-
-
 
 @end
