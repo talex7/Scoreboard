0413d1160facc9471c27565b2c67aa8b08d656b8
talex7 <talexanian@gmail.com>
2016-11-29 13:49:32 -0500

    began player creation

--- Scoreboard/ViewController.m
+++ Scoreboard/ViewController.m
@@ -8,8 +8,13 @@
 
 #import "ViewController.h"
 #import "GameManager.h"
+#import "PageViewController.h"
+#import "AppDelegate.h"
 
 @interface ViewController ()
+
+@property (nonatomic) NSInteger noOfPlayers;
+@property (nonatomic) NSManagedObjectContext *moc;
 
 @end
 
@@ -23,12 +28,60 @@
     self.playerPicker.delegate = self;
     
     GameManager *gm = [GameManager new];
+    
+    
+    AppDelegate *appDelegate = (AppDelegate*)([[UIApplication sharedApplication] delegate]);
+    
+    self.moc = appDelegate.persistentContainer.viewContext;
+                                   
+    
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+
+
+#pragma mark - Segues
+-(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    PageViewController *pVC = (PageViewController*)[segue destinationViewController];
+    
+    NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Player"];
+    NSInteger idNo = 01;
+    [request setPredicate:[NSPredicate predicateWithFormat:@"id == %@", idNo]];
+    
+    NSError *error = nil;
+    NSArray *results = [self.moc executeFetchRequest:request error:&error];
+    if (!results) {
+        Player *player = [NSEntityDescription insertNewObjectForEntityForName:@"Player" inManagedObjectContext:self.moc];
+        results = [results arrayByAddingObject:player];
+//        NSLog(@"Error fetching Employee objects: %@\n%@", [error localizedDescription], [error userInfo]);
+//        abort();
+    }
+    
+    if (self.noOfPlayers == 2) {
+        NSFetchRequest *request = [NSFetchRequest fetchRequestWithEntityName:@"Player"];
+        NSInteger idNo = 02;
+        [request setPredicate:[NSPredicate predicateWithFormat:@"id == %@", idNo]];
+        
+        NSError *error = nil;
+        NSArray *results2 = [self.moc executeFetchRequest:request error:&error];
+        if (!results2) {
+            Player *player = [NSEntityDescription insertNewObjectForEntityForName:@"Player" inManagedObjectContext:self.moc];
+            results = [results arrayByAddingObject:player];
+            //        NSLog(@"Error fetching Employee objects: %@\n%@", [error localizedDescription], [error userInfo]);
+            //        abort();
+        } else {
+            results = [results arrayByAddingObjectsFromArray:results2];
+        }
+    }
+    [pVC.players addObjectsFromArray:results];
+     
+    pVC.pageIndex = 0;
 }
 
 
@@ -46,9 +99,11 @@
     switch (row) {
         case 0:
             rowLabel = @"Single Player";
+            self.noOfPlayers = 1;
             return rowLabel;
         case 1:
             rowLabel = @"2 Players";
+            self.noOfPlayers = 2;
             return rowLabel;
     }
     return rowLabel;
